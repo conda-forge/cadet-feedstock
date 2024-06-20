b6d0a0011fc1af9d7c66fb2a6b38744f44df5414
Johannes Schmölder <j.schmoelder@fz-juelich.de>
2024-06-20 10:22:46 +0200

    Add option to build using MKL

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,22 @@
 mkdir build
 cd build
 
+:: Use environment variables set by conda to detect build variants
+if "%blas_impl%" == "mkl" (
+    echo Building with MKL support
+    set "BLAS_OPTION=Intel10_64lp_seq"
+) else (
+    echo Building with OpenBLAS/LAPACK support
+    set "BLAS_OPTION=Generic"
+)
+
 cmake ^
     -G "NMake Makefiles" ^
     -DCMAKE_PREFIX_PATH=%PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DENABLE_CADET_MEX=OFF ^
+    -DBLA_VENDOR=%BLAS_OPTION% ^
     ..
 
 nmake
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,22 @@
+#!/bin/bash
+
 mkdir build && cd build
+
+# Use environment variables set by conda to detect build variants
+if [[ "$blas_impl" == "mkl" ]]; then
+    echo "Building with MKL support"
+    BLAS_OPTION="Intel10_64lp_seq"
+else
+    echo "Building with OpenBLAS/LAPACK support"
+    BLAS_OPTION="Generic"
+fi
+
 cmake \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_INSTALL_LIBDIR=lib \
-    -DCMAKE_BUILD_TYPE=Release \
     -DENABLE_CADET_MEX=OFF \
+    -DBLA_VENDOR=$BLAS_OPTION \
+    -DCMAKE_BUILD_TYPE=Release \
     ..
 make install -j $CPU_COUNT
