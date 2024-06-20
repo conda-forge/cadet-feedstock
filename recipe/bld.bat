0e57da6f60505b01bc1be312b95a70201ddc4ba2
Johannes Schmölder <j.schmoelder@fz-juelich.de>
2024-06-20 10:12:32 +0200

    Use flags for MKL

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,22 @@
 mkdir build
 cd build
 
+:: Use environment variables set by conda to detect build variants
+if "%blas_impl%" == "true" (
+    echo Building with MKL support
+    set "BLAS_OPTION=Intel10_64lp_seq"
+) else (
+    echo Building with OpenBLAS/LAPACK support
+    set "BLAS_OPTION=Generic"
+)
+
 cmake ^
     -G "NMake Makefiles" ^
     -DCMAKE_PREFIX_PATH=%PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DENABLE_CADET_MEX=OFF ^
-    -DBLA_VENDOR=Intel10_64lp_seq ^
+    -DBLA_VENDOR=%BLAS_OPTION% ^
     ..
 
 nmake
