02fab263974e719357123d9b9b712ff1198a64da
Samuel Leweke <samuel@leweke.info>
2025-05-02 22:23:09 +0200

    Add cross compilation for ARM64-based Macs
    Uses build system compiler for templateCodeGen target, which is required
    for processing the files for the actual build.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,33 @@
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
+  (
+    mkdir -p build-host
+    pushd build-host
+
+    export CC=$CC_FOR_BUILD
+    export CXX=$CXX_FOR_BUILD
+    export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
+    export PKG_CONFIG_PATH=${PKG_CONFIG_PATH//$PREFIX/$BUILD_PREFIX}
+
+    # Unset them as we're ok with builds that are either slow or non-portable
+    unset CFLAGS
+    unset CXXFLAGS
+
+    cmake .. \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_PREFIX_PATH=$BUILD_PREFIX -DCMAKE_INSTALL_PREFIX=$BUILD_PREFIX \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DENABLE_CADET_MEX=OFF \
+      ..
+
+    # No need to compile everything, just templateCodeGen is sufficient
+    cmake --build . --target templateCodeGen --parallel ${CPU_COUNT} --config Release
+
+    ls -lah src/build-tools/
+  )
+fi
 mkdir build && cd build
+ls -lah ../build-host/src/build-tools
 cmake \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
